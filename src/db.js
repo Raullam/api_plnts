a248dff7ff3785fcbe412ea810f64e67da518034
RaulRLM <raullama@paucasesnovescifp.com>
2025-03-19 09:56:26 +0100

    3 commit

--- src/db.js
+++ src/db.js
@@ -1,13 +1,19 @@
 // db.js
 import mysql from 'mysql2' // Usamos mysql2 (sin promesas)
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from './config.js'
+import {
+  DB_HOST,
+  DB_DATABASE,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT,
+} from './config.js'
 
 // Crea el pool de conexiones (sin promesas)
 const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_NAME,
+  database: DB_DATABASE,
   port: DB_PORT,
 })
 
