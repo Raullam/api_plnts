fe813879e7ec6b03cac84d1d30af635f32ddb6c7
RaulRLM <raullama@paucasesnovescifp.com>
2025-04-29 20:25:23 +0200

    modificacions a cofres.js s'ha afegit el auth per verifica el token

--- src/routes/cofres.js
+++ src/routes/cofres.js
@@ -1,45 +1,55 @@
 // routes/cofres.js
 import express from 'express'
 import db from '../db.js' // afegit
+import { auth } from 'google-auth-library'
 const router = express.Router()
 
 /**
  * @swagger
- * tags:
- *   name: Cofres
- *   description: Endpoints per a la gestió dels cofres
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
  */
+
 /**
  * @swagger
- * /cofres/{id}:
+ * /cofrees/{id}:
  *   get:
- *     summary: Obtener cofres de un usuario por ID
+ *     summary: Obté els cofres d’un usuari
+ *     description: Retorna tots els cofres associats a un usuari identificat pel seu ID. Requereix autenticació amb token JWT.
  *     tags:
  *       - Cofres
+ *     security:
+ *       - bearerAuth: []
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
  *         schema:
  *           type: integer
- *         description: ID del usuario
+ *         description: ID de l’usuari per obtenir els seus cofres.
  *     responses:
  *       200:
- *         description: Cofres del usuario encontrados
+ *         description: Cofres trobats correctament.
  *         content:
  *           application/json:
  *             schema:
  *               type: array
  *               items:
- *                 type: object
+ *                 $ref: '#/components/schemas/Cofre'
+ *       401:
+ *         description: No autoritzat. Cal un token vàlid.
  *       404:
- *         description: Cofre no encontrado
+ *         description: Cofre no trobat.
  *       500:
- *         description: Error del servidor
+ *         description: Error intern del servidor.
  */
 
 // Ruta GET /cofres/:userId
-router.get('/:id', (req, res) => {
+router.get('/:id', auth, (req, res) => {
   const { id } = req.params
   const query = 'SELECT * FROM cofrees WHERE idusuari = ?'
   db.query(query, [id], (err, result) => {
