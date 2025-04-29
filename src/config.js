2cadc9ca2cc4fe57cb877993e99b06236add02e1
RaulRLM <raullama@paucasesnovescifp.com>
2025-04-29 14:02:32 +0200

    cambio de einpoint de orden solo visual

--- src/config.js
+++ src/config.js
@@ -5,4 +5,3 @@ export const DB_USER = process.env.DB_USER || 'test'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'test'
 export const DB_NAME = process.env.DB_NAME || 'appplantes2'
 export const DB_PORT = process.env.DB_PORT || 3306
-export const SENDGRID_API_KEY = SG.TU_API_KEY_REAL_AQUI
