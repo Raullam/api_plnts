430939b28130348f39617354afac53b01f44ed2b
RaulRLM <raullama@paucasesnovescifp.com>
2025-04-29 11:43:13 +0200

    se ha añadido el swagger en cofres.js

--- src/routes/cofres.js
+++ src/routes/cofres.js
@@ -2,6 +2,32 @@
 import express from 'express'
 import db from '../db.js' // afegit
 const router = express.Router()
+/**
+ * @swagger
+ * /cofres/{id}:
+ *   get:
+ *     summary: Obtener cofres de un usuario por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del usuario
+ *     responses:
+ *       200:
+ *         description: Cofres del usuario encontrados
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *       404:
+ *         description: Cofre no encontrado
+ *       500:
+ *         description: Error del servidor
+ */
 
 // Ruta GET /cofres/:userId
 router.get('/:id', (req, res) => {
