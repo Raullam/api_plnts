8056500296eccf6c9bd7ce847d40c22fbc3089d0
RaulRLM <raullama@paucasesnovescifp.com>
2025-05-15 20:53:52 +0200

    s'ha modificat todo el archivo matchmakking.js para mejorar la tabla

--- src/routes/matchmaking.js
+++ src/routes/matchmaking.js
@@ -53,42 +53,67 @@ const router = express.Router()
  */
 
 // Ruta para agregar un usuario a la lista de espera
-router.post('/agregar', auth, (req, res) => {
-  const {
-    nom,
-    correu,
-    contrasenya,
-    edat,
-    nacionalitat,
-    codiPostal,
-    imatgePerfil,
-    btc,
-    admin,
-    superadmin,
-    LE,
-    nivell,
-  } = req.body
+// router.post('/agregar', auth, (req, res) => {
+//   const {
+//     nom,
+//     correu,
+//     contrasenya,
+//     edat,
+//     nacionalitat,
+//     codiPostal,
+//     imatgePerfil,
+//     btc,
+//     admin,
+//     superadmin,
+//     LE,
+//     nivell,
+//   } = req.body
+
+//   const query = `
+//     INSERT INTO matchmaking_usuaris (nom, correu, contrasenya, edat, nacionalitat, codiPostal, imatgePerfil, btc, admin, superadmin, LE, nivell, creado_en)
+//     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())`
+
+//   db.query(
+//     query,
+//     [
+//       nom,
+//       correu,
+//       contrasenya,
+//       edat,
+//       nacionalitat,
+//       codiPostal,
+//       imatgePerfil,
+//       btc,
+//       admin,
+//       superadmin,
+//       LE,
+//       nivell,
+//     ],
+//     (error, result) => {
+//       if (error) {
+//         return res.status(500).json({
+//           message: 'Error al agregar usuario a la lista de espera',
+//           error,
+//         })
+//       }
+//       res.status(200).json({
+//         message: 'Usuario agregado a la lista de espera',
+//         userId: result.insertId,
+//       })
+//     },
+//   )
+// })
+
+router.post('/afegir_usuari_a_la_llista_matchmaking', auth, (req, res) => {
+  const { correu, nacionalitat, codiPostal, nivell } = req.body
 
   const query = `
-    INSERT INTO matchmaking_usuaris (nom, correu, contrasenya, edat, nacionalitat, codiPostal, imatgePerfil, btc, admin, superadmin, LE, nivell, creado_en)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())`
+    INSERT INTO matchmaking_usuaris (correu, nacionalitat, codiPostal, nivell)
+    VALUES (?, ?, ?, ?)`
 
   db.query(
     query,
-    [
-      nom,
-      correu,
-      contrasenya,
-      edat,
-      nacionalitat,
-      codiPostal,
-      imatgePerfil,
-      btc,
-      admin,
-      superadmin,
-      LE,
-      nivell,
-    ],
+    [correu, nacionalitat, codiPostal, nivell],
     (error, result) => {
       if (error) {
         return res.status(500).json({
@@ -141,8 +166,22 @@ router.post('/agregar', auth, (req, res) => {
  */
 
 // Ruta para obtener los usuarios que están en espera
-router.get('/usuaris_a_la_llista_de_matchmaking', auth, (req, res) => {
-  const query = 'SELECT * FROM matchmaking_usuaris'
+// router.get('/usuaris_a_la_llista_de_matchmaking', auth, (req, res) => {
+//   const query = 'SELECT * FROM matchmaking_usuaris'
+
+//   db.query(query, (error, results) => {
+//     if (error) {
+//       return res
+//         .status(500)
+//         .json({ message: 'Error al obtener la lista de espera', error })
+//     }
+//     res.status(200).json(results)
+//   })
+// })
+
+// Ruta para obtener los usuarios que están en espera
+router.get('/usuaris_a_la_llista_de_matchmaking2', auth, (req, res) => {
+  const query = 'SELECT * FROM matchmaking_usuaris2'
 
   db.query(query, (error, results) => {
     if (error) {
@@ -153,7 +192,6 @@ router.get('/usuaris_a_la_llista_de_matchmaking', auth, (req, res) => {
     res.status(200).json(results)
   })
 })
-
 /**
  * @swagger
  * /eliminar_usuari_de_la_llista_matchmaking/{id}:
@@ -176,12 +214,32 @@ router.get('/usuaris_a_la_llista_de_matchmaking', auth, (req, res) => {
  */
 
 // Ruta para eliminar un usuario de la lista de espera
+// router.delete(
+//   '/eliminar_usuari_de_la_llista_matchmaking/:id',
+//   auth,
+//   (req, res) => {
+//     const { id } = req.params
+//     const query = 'DELETE FROM matchmaking_usuaris WHERE id = ?'
+
+//     db.query(query, [id], (error, result) => {
+//       if (error) {
+//         return res.status(500).json({
+//           message: 'Error al eliminar usuario de la lista de espera',
+//           error,
+//         })
+//       }
+//       res
+//         .status(200)
+//         .json({ message: 'Usuario eliminado de la lista de espera' })
+//     })
+//   },
+// )
 router.delete(
-  '/eliminar_usuari_de_la_llista_matchmaking/:id',
+  '/eliminar_usuari_de_la_llista_matchmaking2/:id',
   auth,
   (req, res) => {
     const { id } = req.params
-    const query = 'DELETE FROM matchmaking_usuaris WHERE id = ?'
+    const query = 'DELETE FROM matchmaking_usuaris2 WHERE id = ?'
 
     db.query(query, [id], (error, result) => {
       if (error) {
@@ -239,20 +297,40 @@ router.delete(
  */
 
 // Ruta para eliminar por correu de la lista de espera
-router.delete('/eliminar-correu/:correu', auth, (req, res) => {
-  const { correu } = req.params
-  const query = 'DELETE FROM matchmaking_usuaris WHERE correu = ?'
+// router.delete('/eliminar-correu/:correu', auth, (req, res) => {
+//   const { correu } = req.params
+//   const query = 'DELETE FROM matchmaking_usuaris WHERE correu = ?'
 
-  db.query(query, [correu], (error, result) => {
-    if (error) {
-      return res.status(500).json({
-        message: 'Error al eliminar usuario de la lista de espera',
-        error,
-      })
-    }
-    res.status(200).json({ message: 'Usuario eliminado de la lista de espera' })
-  })
-})
+//   db.query(query, [correu], (error, result) => {
+//     if (error) {
+//       return res.status(500).json({
+//         message: 'Error al eliminar usuario de la lista de espera',
+//         error,
+//       })
+//     }
+//     res.status(200).json({ message: 'Usuario eliminado de la lista de espera' })
+//   })
+// })
+router.delete(
+  '/eliminar-usuari-llista-matchmaking/:correu',
+  auth,
+  (req, res) => {
+    const { correu } = req.params
+    const query = 'DELETE FROM matchmaking_usuaris2 WHERE correu = ?'
+
+    db.query(query, [correu], (error, result) => {
+      if (error) {
+        return res.status(500).json({
+          message: 'Error al eliminar usuario de la lista de espera',
+          error,
+        })
+      }
+      res
+        .status(200)
+        .json({ message: 'Usuario eliminado de la lista de espera' })
+    })
+  },
+)
 
 // NO LO USAMOS POR AHORA
 // /**
